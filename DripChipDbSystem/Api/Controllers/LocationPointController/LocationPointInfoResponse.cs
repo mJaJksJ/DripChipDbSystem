79f81c7c319f52a59f1b0918f25d8e4767b429c4
mJaJksJ <maksim.m00@mail.ru>
2023-02-03 22:53:22 +0300

    chore: набросок контроллера
    get запрос для точки локации

---
+++ DripChipDbSystem/Api/Controllers/LocationPointController/LocationPointInfoResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace DripChipDbSystem.Api.Controllers.LocationPointController
+{
+    /// <summary>
+    /// Информации о точке локации животных
+    /// </summary>
+    public class LocationPointInfoResponse
+    {
+        /// <summary>
+        /// Идентификатор точки локации
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Географическая широта в градусах
+        /// </summary>
+        public string Latityde { get; set; }
+
+        /// <summary>
+        ///  Географическая долгота в градусах
+        /// </summary>
+        public string Longitudede { get; set;}
+    }
+}
---
+++ DripChipDbSystem/Services/LocationPointService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using DripChipDbSystem.Api.Controllers.LocationPointController;
+
+namespace DripChipDbSystem.Services
+{
+    public class LocationPointService
+    {
+        public async Task<LocationPointInfoResponse> GetLocationPointInfoAsync(long pointId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
